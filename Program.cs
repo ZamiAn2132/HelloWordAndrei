00ae605e29887591a55a88926be9137fa65ef7f8
Andrei Mihai <mihaiandrei_zapuc@yahoo.com>
2023-10-26 15:25:41 +0300

    Update App Alt test

--- Program.cs
+++ Program.cs
@@ -1,6 +1,6 @@
 var builder = WebApplication.CreateBuilder(args);
 var app = builder.Build();
 
-app.MapGet("/", () => "Hello World Andrei  cu CI!");
+app.MapGet("/", () => "Hello World Andrei  cu CsI!MAi facem un mic test");
 
 app.Run();
