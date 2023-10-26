283bae3814d720a6be576e0d242fb356337195a7
Andrei Mihai <mihaiandrei_zapuc@yahoo.com>
2023-10-26 15:31:44 +0300

    Update App With features

--- Program.cs
+++ Program.cs
@@ -1,6 +1,12 @@
 var builder = WebApplication.CreateBuilder(args);
 var app = builder.Build();
 
-app.MapGet("/", () => "Hello World Andrei  cu CsI!MAi facem un mic test");
+app.MapGet("/", () => "Hello World! This is the main page.");
+
+app.MapGet("/andrei", () => "Hello Andrei! Welcome to your web app.");
+
+app.MapGet("/test", () => "This is a test endpoint. Everything seems to be working!");
+
+app.MapGet("/greet/{name}", (string name) => $"Hello {name}, nice to meet you!");
 
 app.Run();
